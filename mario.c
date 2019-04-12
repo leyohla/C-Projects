9e06f95084274a3ae0cfd06bdc6beabf0bc150a8
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-04-12 08:54:15 +0100

    Update mario.c

--- mario.c
+++ mario.c
@@ -12,16 +12,7 @@ while (true) {
     
    /* for (i=1; i<8; i++) {
         printf("#"); */
-    /*
-    if (i == "1") printf("#");
-    if (i == "2") printf("##");
-    if (i == "3") printf("###");
-    if (i == "4") printf("####");
-    if (i == "5") printf("#####");
-    if (i == "6") printf("######");
-    if (i == "7") printf("#######");
-    if (i == "8") printf("########"); 
-    */
+  
     {   
     if (i <= 8 && i >= 1) {
             printf("Height: %i\n", i); 
