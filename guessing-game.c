45c7c2fd7e1ba365e7a566f49303a8b62dbc7dcd
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-04-29 09:42:18 +0100

    Create guessing-game.c

---
+++ guessing-game.c
@@ -0,0 +1,34 @@
+/*
+ * The following program will act as a guessing game in which the user has eight tries to guess a randomly generated number. 
+ The program will tell the user each time whether he guessed high or low.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand (time(NULL) );
+    //int random = 0; int a = 0; 
+    int i; int n;
+    int random = (rand() % 99)+1;
+    
+    for (i=0; i<8; i++) {
+        printf("Guess the random number\n");
+        scanf("%d", &n);
+        if (n == random) {
+            printf("you've guessed correctly\n");
+            break;
+            //exit(1);
+        }
+        else if (n < random){
+            printf("the number is too low\n");
+        }
+        else {
+            printf("the number is too high\n");
+        }
+    }
+    if(n != random) {
+        printf("wrong, you've failed.\n");
+       
+    }
+}
