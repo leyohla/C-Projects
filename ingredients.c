e1d873f13bedf4814ab6bd8a24a9a7f2659da6c9
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-05-21 10:28:08 +0100

    Update ingredients.c

--- ingredients.c
+++ ingredients.c
@@ -1,8 +1,8 @@
-Problem set: You plan to make a delicious meal and want to take the money you need to buy the ingredients. Fortunately you know in 
+/* Problem set: You plan to make a delicious meal and want to take the money you need to buy the ingredients. Fortunately you know in 
 advance the price per pound of each ingredient as well as the exact amount you need. The program should read in the number of ingredients 
 (up to a maximum of 10 ingredients), then for each ingredient the price per pound. Finally your program should read the weight necessary 
 for the recipe (for each ingredient in the same order). Your program should calculate the total cost of these purchases, then display it 
-with 6 decimal places.
+with 6 decimal places. */
 
 #include <stdio.h>
 
